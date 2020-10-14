1dfaeae661078bfe054cb903449951ce682bf313
Georgi Gradev <72765831+GeorgiGradev@users.noreply.github.com>
2020-10-14 23:45:04 +0300

    Add files via upload

---
+++ Programming Basics/3.4. EXAM - Conditional Statements/02.08. Godzilla vs. Kong/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace _02._08._Godzilla_vs._Kong
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double budget = double.Parse(Console.ReadLine());
+            int actors = int.Parse(Console.ReadLine());
+            double clothesPrice = double.Parse(Console.ReadLine());
+
+            if (actors > 150)
+            {
+                clothesPrice = clothesPrice * 0.90;
+            }
+
+            else
+            {
+                clothesPrice = clothesPrice * 1.0;
+            }
+                
+
+            double totalClothesPrice = clothesPrice * actors;
+            double costs = totalClothesPrice + (budget * 0.10);
+
+            if (costs > budget)
+            {
+                double difference = costs - budget;
+                Console.WriteLine("Not enough money!");
+                Console.WriteLine($"Wingard needs {difference:F2} leva more.");
+            }
+            else
+            {
+                double difference = budget - costs;
+                Console.WriteLine("Action!");
+                Console.WriteLine($"Wingard starts filming with {difference:F2} leva left.");
+            }
+        }
+    }
+}
---
+++ Programming Basics/3.5. C BOOK -  Simple Conditions/07. Sum Seconds/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace _07._Sum_Seconds
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int A = int.Parse(Console.ReadLine());
+            int B = int.Parse(Console.ReadLine());
+            int C = int.Parse(Console.ReadLine());
+
+            int totalMinutes = (A + B + C) / 60;
+            int totalSeconds = (A + B + C) % 60;
+
+            Console.WriteLine($"{totalMinutes}:{totalSeconds:D2}");
+
+
+        }
+
+    }
+}
