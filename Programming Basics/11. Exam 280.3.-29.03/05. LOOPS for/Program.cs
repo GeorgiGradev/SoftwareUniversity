10f18786b3e9990c3b8c5a80ec691ce953b4e569
Georgi Gradev <72765831+GeorgiGradev@users.noreply.github.com>
2020-10-15 00:18:34 +0300

    Add files via upload

---
+++ Programming Basics/11. Exam 280.3.-29.03/05. LOOPS for/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace _05._LOOPS_for
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double capacity = double.Parse(Console.ReadLine());
+            string suitcaseCommand = Console.ReadLine();
+
+            int counter = 0;
+            double totalVolume = 0;
+            bool isSpace = true;
+
+            while (suitcaseCommand != "End")
+            {
+                double suitcaseVolume = double.Parse(suitcaseCommand);
+                counter++;
+
+                if (counter % 3 == 0)
+                {
+                    suitcaseVolume *= 1.1;
+                }
+
+                totalVolume += suitcaseVolume;
+
+                if (totalVolume > capacity)
+                {
+                    counter--;
+                    Console.WriteLine("No more space!");
+                    isSpace = false;
+                    break;
+                }
+                suitcaseCommand = Console.ReadLine();
+            }
+            if (isSpace)
+            {
+                Console.WriteLine("Congratulations! All suitcases are loaded!");
+            }
+            Console.WriteLine($"Statistic: {counter} suitcases loaded.");
+        }
+    }
+}
---
+++ Programming Basics/11. Exam 280.3.-29.03/06. Nested loops/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+
+namespace _06._Nested_loops
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double days = double.Parse(Console.ReadLine());
+            string sportCommand = "";
+
+            double totalMoney = 0;
+            double dayWinCounter = 0;
+            double dayLoseCounter = 0;
+
+            for (int i = 1; i <= days; i++)
+            {
+                sportCommand = Console.ReadLine();
+                double winCounter = 0;
+                double loseCounter = 0;
+                double dayMoney = 0;
+                while (sportCommand != "Finish")
+                {
+                    string result = Console.ReadLine();
+                    if (result == "win")
+                    {
+                        dayMoney += 20;
+                        winCounter++;
+                    }
+                    else if (result == "lose")
+                    {
+                        loseCounter++;
+                    }
+                    sportCommand = Console.ReadLine();
+                }
+                if (winCounter > loseCounter)
+                {
+                    dayMoney *= 1.1;
+                    dayWinCounter++;
+                }
+                else
+                {
+                    dayLoseCounter++;
+                }
+                totalMoney += dayMoney;
+            }
+            if (dayWinCounter > dayLoseCounter)
+            {
+                totalMoney *= 1.2;
+                Console.WriteLine($"You won the tournament! Total raised money: {totalMoney:f2}");
+            }
+            else
+            {
+                Console.WriteLine($"You lost the tournament! Total raised money: {totalMoney:f2}");
+            }
+        }
+    }
+}
