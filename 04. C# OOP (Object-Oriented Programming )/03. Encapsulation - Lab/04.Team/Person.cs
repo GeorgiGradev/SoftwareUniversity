7c074599e8cb398a65e9b17500dbd09768e0acac
Georgi Gradev <georgi.d.gradev@gmail.com>
2020-10-31 21:01:59 +0200

    Add files via upload

---
+++ 04. C# OOP (Object-Oriented Programming )/03. Encapsulation - Lab/04.Team/Person.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+using System.Text;
+
+namespace PersonsInfo
+{
+    public class Person
+    {
+        private string firstName;
+        private string lastName;
+        private int age;
+        private decimal salary;
+
+
+
+        public Person (string firstName, string lastName, int age, decimal salary)
+        {
+            this.FirstName = firstName;
+            this.LastName = lastName;
+            this.Age = age;
+            this.Salary = salary;
+        }
+
+        public string FirstName
+        {
+            get
+            {
+                return this.firstName;
+            }
+            private set
+            {
+                if (NameValidation(value))
+                {
+                    this.firstName = value;
+                }
+                else
+                {
+                    throw new ArgumentException("First name cannot contain fewer than 3 symbols!");
+                }
+            }
+        }
+
+        public string LastName
+        {
+            get
+            {
+                return this.lastName;
+            }
+            private set
+            {
+                if (NameValidation(value))
+                {
+                    this.lastName = value;
+                }
+                else
+                {
+                    throw new ArgumentException("Last name cannot contain fewer than 3 symbols!");
+                }    
+            }
+        }
+
+        public int Age
+        {
+            get
+            {
+                return this.age;
+            }
+            set
+            {
+                if (value > 0)
+                {
+                    this.age = value;
+                }
+                else
+                {
+                    throw new ArgumentException("Age cannot be zero or a negative integer!");
+                }
+            }
+        }
+        public decimal Salary
+        {
+            get
+            {
+                return this.salary;
+            }
+            set
+            {
+                if (value >= 460)
+                {
+                    this.salary = value;
+                }
+                else
+                {
+                    throw new ArgumentException("Salary cannot be less than 460 leva!");
+                }
+            }
+        }
+
+        public override string ToString()
+        {
+            return $"{this.FirstName} {this.LastName} receives {this.Salary:f2} leva."; 
+        }
+
+
+        public bool NameValidation (string name)
+        {
+            return name?.Length > 2;
+        }
+
+    }
+}
---
+++ 04. C# OOP (Object-Oriented Programming )/03. Encapsulation - Lab/04.Team/Team.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PersonsInfo
+{
+    public class Team
+    {
+        private string name;
+        private List<Person> firstTeam;
+        private List<Person> reserveTeam;
+
+        public Team(string name)
+        {
+            this.name = name;
+            this.firstTeam = new List<Person>();
+            this.reserveTeam = new List<Person>();
+        }
+
+       public IReadOnlyCollection<Person> FirstTeam
+        {
+            get
+            {
+                return this.firstTeam.AsReadOnly();
+            }
+        }
+
+        public IReadOnlyCollection<Person> ReserveTeam
+        {
+            get
+            {
+                return this.reserveTeam.AsReadOnly();
+            }  
+        }
+
+
+
+        public void AddPlayer(Person person)
+        {
+            if (person.Age < 40)
+            {
+               firstTeam.Add(person);
+            }
+            else
+            {
+                reserveTeam.Add(person);
+            }
+        }
+
+
+        public override string ToString()
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.AppendLine($"First team has {firstTeam.Count} players.");
+            sb.AppendLine($"Reserve team has {reserveTeam.Count} players.");
+            return sb.ToString().TrimEnd();
+        }
+    }
+}
