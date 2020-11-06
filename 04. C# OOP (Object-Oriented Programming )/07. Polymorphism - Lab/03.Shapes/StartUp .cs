efc603737cfd98c1ef1b64c3dd131196dddaad78
Georgi Gradev <georgi.d.gradev@gmail.com>
2020-11-06 15:29:53 +0200

    Add files via upload

---
+++ 04. C# OOP (Object-Oriented Programming )/07. Polymorphism - Lab/03.Shapes/StartUp .cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Shapes
+{
+   public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            Shape rectange = new Rectangle(10, 4);
+            Shape circle = new Circle(3);
+
+            Console.WriteLine(rectange.CalculateArea());
+            Console.WriteLine(rectange.CalculatePerimeter());
+            Console.WriteLine(rectange.Draw());
+            Console.WriteLine(circle.CalculateArea());
+            Console.WriteLine(rectange.CalculatePerimeter());
+            Console.WriteLine(circle.Draw());
+        }
+    }
+}
