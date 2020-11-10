3ab1eba4989cf3344110a494d4d4c9acdd6b1a85
Georgi Gradev <georgi.d.gradev@gmail.com>
2020-11-10 14:54:10 +0200

    Add files via upload

---
+++ 04. C# OOP/10. Solid - Exercise/01. Logger/Interfaces/IAppender.cs
@@ -0,0 +1,15 @@
+﻿namespace _1.Logger.Interfaces
+{
+    using Enums;
+
+    public interface IAppender
+    {
+        ILayout Layout { get; }
+
+        ReportLevel ReportLevel { get; set; }
+
+        void Append(string datetime, string reportLevel, string message);
+
+        bool IsMessageValidLevel(string messageLevel);
+    }
+}
