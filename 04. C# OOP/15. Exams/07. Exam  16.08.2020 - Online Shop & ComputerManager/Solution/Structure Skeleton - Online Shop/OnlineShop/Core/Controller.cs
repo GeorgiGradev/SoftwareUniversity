6b4654b894339d73cc89d8f85577d78f92fec2f0
Georgi Gradev <georgi.d.gradev@gmail.com>
2020-12-02 09:55:21 +0200

    Add files via upload

---
+++ 04. C# OOP/15. Exams/07. Exam  16.08.2020 - Online Shop & ComputerManager/Solution/Structure Skeleton - Online Shop/OnlineShop/Core/Controller.cs
@@ -0,0 +1,201 @@
+﻿namespace OnlineShop.Core
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+
+    using OnlineShop.Common.Constants;
+    using OnlineShop.Models.Products.Components;
+    using OnlineShop.Models.Products.Computers;
+    using OnlineShop.Models.Products.Peripherals;
+
+    public class Controller : IController
+    {
+        private readonly ICollection<IComputer> computers;
+        private readonly ICollection<IComponent> components;
+        private readonly ICollection<IPeripheral> peripherals;
+
+        public Controller()
+        {
+            this.computers = new List<IComputer>();
+            this.components = new List<IComponent>();
+            this.peripherals = new List<IPeripheral>();
+        }
+
+        public string AddComputer(string computerType, int id, string manufacturer, string model, decimal price)
+        {
+            IComputer computer;
+            if (computers.Any(x => x.Id == id))
+            {
+                throw new ArgumentException(ExceptionMessages.ExistingComputerId);
+            }
+            else if (computerType == "DesktopComputer")
+            {
+                computer = new DesktopComputer(id, manufacturer, model, price);
+            }
+            else if (computerType == "Laptop")
+            {
+                computer = new Laptop(id, manufacturer, model, price);
+            }
+            else
+            {
+                throw new ArgumentException(ExceptionMessages.InvalidComputerType);
+            }
+            computers.Add(computer);
+            return string.Format(SuccessMessages.AddedComputer, id);
+        }
+
+        public string AddComponent(int computerId, int id, string componentType, string manufacturer, string model, decimal price, double overallPerformance, int generation)
+        {
+            DoesComputerExist(computerId);
+
+            if (components.Any(x => x.Id == id))
+            {
+                throw new ArgumentException(ExceptionMessages.ExistingComponentId);
+            }
+
+            IComponent component = null;
+            if (componentType == nameof(CentralProcessingUnit))
+            {
+                component = new CentralProcessingUnit(id, manufacturer, model, price, overallPerformance, generation);
+            }
+            else if (componentType == nameof(Motherboard))
+            {
+                component = new Motherboard(id, manufacturer, model, price, overallPerformance, generation);
+            }
+            else if (componentType == nameof(PowerSupply))
+            {
+                component = new PowerSupply(id, manufacturer, model, price, overallPerformance, generation);
+            }
+            else if (componentType == nameof(RandomAccessMemory))
+            {
+                component = new RandomAccessMemory(id, manufacturer, model, price, overallPerformance, generation);
+            }
+            else if (componentType == nameof(SolidStateDrive))
+            {
+                component = new SolidStateDrive(id, manufacturer, model, price, overallPerformance, generation);
+            }
+            else if (componentType == nameof(VideoCard))
+            {
+                component = new VideoCard(id, manufacturer, model, price, overallPerformance, generation);
+            }
+            else
+            {
+                throw new ArgumentException(ExceptionMessages.InvalidComponentType);
+            }
+
+            IComputer computerToBeAddedTo = computers.FirstOrDefault(x => x.Id == computerId);
+
+            computerToBeAddedTo.AddComponent(component);
+            components.Add(component);
+
+            return string.Format(SuccessMessages.AddedComponent, componentType, id, computerId);
+        }
+
+        public string RemoveComponent(string componentType, int computerId)
+        {
+            DoesComputerExist(computerId);
+
+            IComponent componentToRemove = this.components.FirstOrDefault(x => x.GetType().Name == componentType);
+            IComputer computerToRemoveFrom = this.computers.FirstOrDefault(x => x.Id == computerId);
+
+            this.components.Remove(componentToRemove);
+            computerToRemoveFrom.RemoveComponent(componentType);
+
+            return string.Format(SuccessMessages.RemovedComponent, componentType, componentToRemove.Id);
+        }
+
+        public string AddPeripheral(int computerId, int id, string peripheralType, string manufacturer, string model, decimal price, double overallPerformance, string connectionType)
+        {
+            DoesComputerExist(computerId);
+
+            if (peripherals.Any(x => x.Id == id))
+            {
+                throw new ArgumentException(ExceptionMessages.ExistingPeripheralId);
+            }
+
+            IPeripheral peripheral = null;
+            if (peripheralType == nameof(Headset))
+            {
+                peripheral = new Headset(id, manufacturer, model, price, overallPerformance, connectionType);
+            }
+            else if (peripheralType == nameof(Keyboard))
+            {
+                peripheral = new Keyboard(id, manufacturer, model, price, overallPerformance, connectionType);
+            }
+            else if (peripheralType == nameof(Monitor))
+            {
+                peripheral = new Monitor(id, manufacturer, model, price, overallPerformance, connectionType);
+            }
+            else if (peripheralType == nameof(Mouse))
+            {
+                peripheral = new Mouse(id, manufacturer, model, price, overallPerformance, connectionType);
+            }
+            else
+            {
+                throw new ArgumentException(ExceptionMessages.InvalidPeripheralType);
+            }
+
+            IComputer computerToBeAddedTo = computers.FirstOrDefault(x => x.Id == computerId);
+
+            computerToBeAddedTo.AddPeripheral(peripheral);
+            peripherals.Add(peripheral);
+            return string.Format(SuccessMessages.AddedPeripheral, peripheralType, id, computerId);
+        }
+
+        public string RemovePeripheral(string peripheralType, int computerId)
+        {
+            DoesComputerExist(computerId);
+
+            IPeripheral peripheralToRemove = peripherals.FirstOrDefault(x => x.GetType().Name == peripheralType);
+            IComputer computerToRemoveFrom = computers.FirstOrDefault(x => x.Id == computerId);
+
+            computerToRemoveFrom.RemovePeripheral(peripheralType);
+            return string.Format(SuccessMessages.RemovedPeripheral, peripheralType, peripheralToRemove.Id);
+        }
+
+        public string BuyComputer(int id)
+        {
+            DoesComputerExist(id);
+
+            var computerToRemove = computers.FirstOrDefault(x => x.Id == id);
+            string removedComputer = computerToRemove.ToString();
+            this.computers.Remove(computerToRemove);
+            return removedComputer;
+        }
+
+        public string BuyBest(decimal budget)
+        {
+            IComputer bestComputer = this.computers
+                .Where(x => x.Price <= budget)
+                .OrderByDescending(x => x.OverallPerformance)
+                .FirstOrDefault();
+
+            if (bestComputer == null)
+            {
+                throw new ArgumentException(string.Format(ExceptionMessages.CanNotBuyComputer, budget));
+            }
+
+            this.computers.Remove(bestComputer);
+            return bestComputer.ToString();
+        }
+
+        public string GetComputerData(int id)
+        {
+            DoesComputerExist(id);
+
+            var computer = computers.FirstOrDefault(x => x.Id == id);
+            return computer.ToString();
+        }
+
+
+
+        private void DoesComputerExist(int id)
+        {
+            if (!computers.Any(x => x.Id == id))
+            {
+                throw new ArgumentException(ExceptionMessages.NotExistingComputerId);
+            }
+        }
+    }
+}
