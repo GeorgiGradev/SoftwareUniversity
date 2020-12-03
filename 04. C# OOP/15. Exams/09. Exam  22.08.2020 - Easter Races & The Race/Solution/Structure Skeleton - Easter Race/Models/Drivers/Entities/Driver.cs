ad125707ca49217cb0cbfc11c9c9a585283cb7bb
Georgi Gradev <georgi.d.gradev@gmail.com>
2020-12-03 22:02:43 +0200

    Add files via upload

---
+++ 04. C# OOP/15. Exams/09. Exam  22.08.2020 - Easter Races & The Race/Solution/Structure Skeleton - Easter Race/Models/Drivers/Entities/Driver.cs
@@ -0,0 +1,49 @@
+﻿namespace EasterRaces.Models.Drivers.Entities
+{
+    using System;
+
+    using EasterRaces.Models.Cars.Contracts;
+    using EasterRaces.Models.Drivers.Contracts;
+    using EasterRaces.Utilities.Messages;
+
+    public class Driver : IDriver
+    {
+        private const int ModelNameSymbols = 5;
+        private string name;
+
+        public Driver(string name)
+        {
+            this.Name = name;
+        }
+
+        public string Name 
+        {
+            get => this.name;
+            private set
+            {
+                if(string.IsNullOrEmpty(value) || value.Length < ModelNameSymbols)
+                {
+                    throw new ArgumentException(string.Format(ExceptionMessages.InvalidName, value, ModelNameSymbols));
+                }
+                this.name = value;
+            }
+        }
+        public ICar Car { get; private set; }
+        public int NumberOfWins { get; private set; }
+        public bool CanParticipate { get; private set; }
+
+        public void AddCar(ICar car)
+        {
+            if (car is null)
+            {
+                throw new ArgumentException(ExceptionMessages.CarInvalid);
+            }
+            this.Car = car;
+            this.CanParticipate = true;
+        }
+        public void WinRace()
+        {
+            this.NumberOfWins++;
+        }
+    }
+}
