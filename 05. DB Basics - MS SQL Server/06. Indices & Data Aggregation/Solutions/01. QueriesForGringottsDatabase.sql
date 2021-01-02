ff515618301c075306d29a3f75a6a677982625ff
Georgi Gradev <georgi.d.gradev@gmail.com>
2021-01-02 18:23:43 +0200

    Update 01. QueriesForGringottsDatabase.sql

--- 05. DB Basics - MS SQL Server/06. Indices & Data Aggregation/Solutions/01. QueriesForGringottsDatabase.sql
+++ 05. DB Basics - MS SQL Server/06. Indices & Data Aggregation/Solutions/01. QueriesForGringottsDatabase.sql
@@ -58,3 +58,69 @@ SELECT
 
 
 --- 6.Deposits Sum for Ollivander Family ---
+SELECT 
+	DepositGroup,
+	SUM(DepositAmount) AS TotalSum
+	FROM WizzardDeposits
+	GROUP BY DepositGroup, MagicWandCreator
+	HAVING MagicWandCreator = 'Ollivander family'
+
+
+--SELECT DepositGroup, SUM(DepositAmount) AS TotalSum	 FROM WizzardDeposits
+--	WHERE MagicWandCreator = 'Ollivander family'
+--	GROUP BY DepositGroup
+
+
+--- 7.Deposits Filter ---
+SELECT 
+	DepositGroup,
+	SUM(DepositAmount) AS TotalSum
+	FROM WizzardDeposits
+	GROUP BY DepositGroup, MagicWandCreator
+	HAVING MagicWandCreator = 'Ollivander family' AND SUM(DepositAmount) < 150000
+	ORDER BY SUM(DepositAmount) DESC
+
+--SELECT * 
+--FROM (SELECT DepositGroup, SUM(DepositAmount) AS TotalSum FROM WizzardDeposits
+--      WHERE MagicWandCreator = 'Ollivander family'
+--      GROUP BY DepositGroup
+--     ) AS DepositGroupsTotalSum
+--WHERE TotalSum < 150000
+--ORDER BY TotalSum DESC
+
+
+--- 8.Deposit Charge ---
+SELECT
+	WizzardDeposits.DepositGroup,
+	WizzardDeposits.MagicWandCreator,
+	MIN(WizzardDeposits.DepositCharge) AS MinDepositCharge
+	FROM WizzardDeposits
+	GROUP BY DepositGroup, MagicWandCreator
+	ORDER BY MagicWandCreator, DepositGroup
+
+
+--SELECT DepositGroup, 
+--       MagicWandCreator, 
+--       MIN(DepositCharge) AS MinDepositCharge 
+--FROM WizzardDeposits
+--GROUP BY DepositGroup, MagicWandCreator
+
+
+--- 9.Age Groups ---
+
+SELECT 
+	AgeGroup,
+	COUNT (*) AS WizzardCount
+	FROM (SELECT
+		CASE 
+			WHEN Age BETWEEN 0 AND 10 THEN '[0-10]'
+         			WHEN Age BETWEEN 11 AND 20 THEN '[11-20]'
+         			WHEN Age BETWEEN 21 AND 30 THEN '[21-30]'
+         			WHEN Age BETWEEN 31 AND 40 THEN '[31-40]'
+         			WHEN Age BETWEEN 41 AND 50 THEN '[41-50]'
+         			WHEN Age BETWEEN 51 AND 60 THEN '[51-60]'
+         			ELSE '[61+]'
+         		END AS AgeGroup
+				FROM WizzardDeposits) AS WizzardAge
+	GROUP BY AgeGroup
+
