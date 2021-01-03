0e981f7426e1606c8316745989b56341318320dc
Georgi Gradev <georgi.d.gradev@gmail.com>
2021-01-03 09:19:14 +0200

    Update 02. QueriesForSoftuniDatabase.sql

--- 05. DB Basics - MS SQL Server/06. Indices & Data Aggregation/Solutions/02. QueriesForSoftuniDatabase.sql
+++ 05. DB Basics - MS SQL Server/06. Indices & Data Aggregation/Solutions/02. QueriesForSoftuniDatabase.sql
@@ -39,3 +39,50 @@ GROUP BY DepartmentID
 
 
 --- 16. Employees Maximum Salaries
+SELECT
+	Employees.DepartmentID,
+	MAX(Employees.Salary) AS MaxSalary
+	FROM Employees
+	GROUP BY Employees.DepartmentID
+	HAVING MAX(Employees.Salary) NOT BETWEEN 30000 AND 70000 
+
+
+--- 17.Employees Count Salaries ---
+SELECT 
+	COUNT(Employees.Salary)
+	FROM Employees
+	WHERE Employees.ManagerID IS NULL
+
+--SELECT COUNT(*) AS Count FROM Employees
+--WHERE ManagerID IS NULL
+
+
+--- 18.*3rd Highest Salary ---
+SELECT 
+	DepartmentID,
+	Salary AS ThirdHighestSalary
+	FROM (SELECT 
+		DepartmentID,
+		Salary,
+		DENSE_RANK() OVER (PARTITION BY DepartmentID ORDER BY Salary DESC) AS [Rank]
+		FROM Employees
+		GROUP BY DepartmentID, Salary
+        ) AS RankSalariesInDepartments
+	WHERE [Rank] = 3
+
+
+--- 19.**Salary Challenge ---
+SELECT TOP(10)
+	e.FirstName,
+	e.LastName,
+	e.DepartmentID
+	FROM Employees as e
+	WHERE e.Salary > 
+		(SELECT 
+		AVG(emp.Salary)
+		FROM Employees AS emp
+		GROUP BY emp.DepartmentID
+		HAVING e.DepartmentID = emp.DepartmentID)
+	ORDER BY e.DepartmentID
+
+	
