191c7413dd42342799a3bda0fd9a62281d48e974
Georgi Gradev <georgi.d.gradev@gmail.com>
2021-01-05 18:07:10 +0200

    Update 1.QueriesForSoftUniDatabase.sql

--- 05. DB Basics - MS SQL Server/07. Functions & Stored Procedures/Solutions/1.QueriesForSoftUniDatabase.sql
+++ 05. DB Basics - MS SQL Server/07. Functions & Stored Procedures/Solutions/1.QueriesForSoftUniDatabase.sql
@@ -11,7 +11,20 @@ USE SoftUni
 		FROM Employees
 		WHERE Salary > 35000
 END
-GO
-EXEC dbo.usp_GetEmployeesSalaryAbove35000
+--EXEC dbo.usp_GetEmployeesSalaryAbove35000
+
 
 --- 2.Employees with Salary Above Number ---
+GO
+CREATE OR ALTER PROC usp_GetEmployeesSalaryAboveNumber (@MinSalary DECIMAL(18,4))
+AS 
+BEGIN
+	SELECT
+		FirstName AS [First Name],
+		LastName AS [LastName]
+		FROM Employees
+		WHERE Salary >= @MinSalary
+END
+GO 
+
+EXEC dbo.usp_GetEmployeesSalaryAboveNumber 48100
