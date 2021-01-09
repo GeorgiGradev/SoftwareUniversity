b2f7939a83303449ae3a6128faac67fbfbf21292
Georgi Gradev <georgi.d.gradev@gmail.com>
2021-01-09 12:48:23 +0200

    Update 01.QueriesForBankDatahase.sql

--- 05. DB Basics - MS SQL Server/08. Triggers & Transactions/Solutions/01.QueriesForBankDatahase.sql
+++ 05. DB Basics - MS SQL Server/08. Triggers & Transactions/Solutions/01.QueriesForBankDatahase.sql
@@ -1,14 +1,14 @@
-USE Bank
+ USE Bank
 
 
 --- 01.Create Table Logs ---
-CREATE TABLE Logs
-(
-	LogId INT PRIMARY KEY IDENTITY NOT NULL,
-	AccountId INT FOREIGN KEY REFERENCES Accounts(Id),
-	OldSum Money,
-	NewSum Money
-)
+--CREATE TABLE Logs
+--(
+--	LogId INT PRIMARY KEY IDENTITY NOT NULL,
+--	AccountId INT FOREIGN KEY REFERENCES Accounts(Id),
+--	OldSum Money,
+--	NewSum Money
+--)
 
 GO
 CREATE OR ALTER TRIGGER tr_AccountChange 
@@ -21,8 +21,47 @@ BEGIN
 		JOIN deleted AS d ON i.Id = d.Id
 END
 GO
-
+ 
 
 --- 2.Create Table Emails ---
+--CREATE TABLE NotificationEmail
+--(
+--	Id INT PRIMARY KEY IDENTITY,
+--	Recipient INT FOREIGN KEY REFERENCES Accounts(Id),
+--	[Subject] NVARCHAR(50),
+--	Body NVARCHAR(MAX)
+--)
+
+GO
+CREATE OR ALTER TRIGGER tr_SendEmailOnUpdate
+ON Logs FOR INSERT
+AS
+BEGIN
+	INSERT INTO NotificationEmail (Recipient, [Subject], Body)
+	SELECT 
+	i.AccountId, 
+		CONCAT(
+			'Balance change for account: ',
+			i.AccountId),
+		CONCAT(
+			'On ',
+			FORMAT(GETDATE(),
+			'MMM dd yyyy HH:mmtt'),
+			' your balance was changed from ',
+			i.OldSum, ' to ',
+			i.NewSum,
+			'.')
+	FROM inserted AS i
+END
+GO
+
+--UPDATE Accounts
+--SET Balance += 100
+--WHERE Id = 1;
+
+--SELECT * FROM Logs
+--SELECT * FROM NotificationEmail
 
 
+
+--- 3.Deposit Money ---
