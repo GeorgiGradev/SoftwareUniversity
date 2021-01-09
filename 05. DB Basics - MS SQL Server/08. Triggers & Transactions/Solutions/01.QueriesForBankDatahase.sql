827ae4f68917e887f928a18033b8e30d3fb2436f
Georgi Gradev <georgi.d.gradev@gmail.com>
2021-01-09 11:54:18 +0200

    Update 01.QueriesForBankDatahase.sql

--- 05. DB Basics - MS SQL Server/08. Triggers & Transactions/Solutions/01.QueriesForBankDatahase.sql
+++ 05. DB Basics - MS SQL Server/08. Triggers & Transactions/Solutions/01.QueriesForBankDatahase.sql
@@ -1,2 +1,28 @@
 USE Bank
 
+
+--- 01.Create Table Logs ---
+CREATE TABLE Logs
+(
+	LogId INT PRIMARY KEY IDENTITY NOT NULL,
+	AccountId INT FOREIGN KEY REFERENCES Accounts(Id),
+	OldSum Money,
+	NewSum Money
+)
+
+GO
+CREATE OR ALTER TRIGGER tr_AccountChange 
+ON Accounts FOR UPDATE
+AS
+BEGIN
+	INSERT INTO Logs (AccountId, OldSum, NewSum)
+	SELECT  i.AccountHolderId, d.Balance, i.Balance
+		FROM inserted as i
+		JOIN deleted AS d ON i.Id = d.Id
+END
+GO
+
+
+--- 2.Create Table Emails ---
+
+
