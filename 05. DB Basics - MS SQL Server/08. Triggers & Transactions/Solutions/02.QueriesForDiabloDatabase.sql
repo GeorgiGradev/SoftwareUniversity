37d3e8f2b6435fb1808a9bcbc495cdab649c7eb5
Georgi Gradev <georgi.d.gradev@gmail.com>
2021-01-09 18:27:42 +0200

    Update 02.QueriesForDiabloDatabase.sql

--- 05. DB Basics - MS SQL Server/08. Triggers & Transactions/Solutions/02.QueriesForDiabloDatabase.sql
+++ 05. DB Basics - MS SQL Server/08. Triggers & Transactions/Solutions/02.QueriesForDiabloDatabase.sql
@@ -101,3 +101,77 @@ END
 --Display results
 
 
+--- 7.*Massive Shopping ---
+GO
+CREATE PROC usp_BuyItemsFromLevelToLevel @username VARCHAR(50), 
+									     @gameName VARCHAR(50), 
+										 @startItemsLevel INT , 
+										 @endItemsLevel INT 
+AS
+BEGIN
+
+	DECLARE @userGameId INT = (SELECT ug.Id
+							   FROM UsersGames AS ug
+							   JOIN Users AS u ON ug.UserId = u.Id
+							   JOIN Games AS g ON ug.GameId = g.Id
+							   WHERE u.Username = @username AND g.Name = @gameName
+							  )
+	
+	DECLARE @userGameLevel INT = (SELECT [Level]
+	                              FROM UsersGames
+	                              WHERE Id = @userGameId
+								 )
+
+	DECLARE @money MONEY = (SELECT Cash
+						    FROM UsersGames
+						    WHERE Id = @userGameId
+						   )
+	DECLARE @itemsValue MONEY = (SELECT SUM(Price)
+								 FROM Items
+								 WHERE MinLevel BETWEEN @startItemsLevel AND @endItemsLevel
+								)
+	
+	IF @money >= @itemsValue AND @userGameLevel >= @endItemsLevel
+	
+	  BEGIN
+	    BEGIN TRANSACTION
+	    
+		UPDATE UsersGames
+	    SET Cash -= @itemsValue
+	    WHERE Id = @userGameId
+	    
+		IF (@@ROWCOUNT != 1)
+	      BEGIN
+	        ROLLBACK
+	        RAISERROR ('Could not make payment', 16, 1)
+	      END
+	    ELSE
+	      BEGIN
+	        INSERT INTO UserGameItems (ItemId, UserGameId)
+	          (SELECT Id, @userGameId
+	           FROM Items
+	           WHERE MinLevel BETWEEN @startItemsLevel AND @endItemsLevel
+			  )
+	
+	        IF (SELECT COUNT(*) FROM Items
+	            WHERE MinLevel BETWEEN @startItemsLevel AND @endItemsLevel
+			   ) != @@ROWCOUNT
+	          BEGIN
+	            ROLLBACK;
+	            RAISERROR ('Could not buy items', 16, 1)
+	          END
+	        ELSE COMMIT;
+	      END
+	  END
+END
+
+EXEC dbo.usp_BuyItemsFromLevelToLevel 'Stamat', 'Safflower', 11, 12
+EXEC dbo.usp_BuyItemsFromLevelToLevel 'Stamat', 'Safflower', 19, 21
+
+SELECT i.Name AS [Item Name]
+FROM UserGameItems AS ugi
+JOIN Items AS i ON i.Id = ugi.ItemId
+JOIN UsersGames AS ug ON ug.Id = ugi.UserGameId
+JOIN Games AS g ON g.Id = ug.GameId
+WHERE g.Name = 'Safflower'
+ORDER BY [Item Name]
