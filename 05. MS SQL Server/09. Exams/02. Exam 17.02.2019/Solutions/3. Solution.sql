1ba4ff9e70401e1ec0bb2ec4f6098690a5d7d691
Georgi Gradev <georgi.d.gradev@gmail.com>
2021-01-14 17:47:28 +0200

    Update 3. Solution.sql

--- 05. MS SQL Server/09. Exams/02. Exam 17.02.2019/Solutions/3. Solution.sql
+++ 05. MS SQL Server/09. Exams/02. Exam 17.02.2019/Solutions/3. Solution.sql
@@ -34,3 +34,61 @@ DELETE
 DELETE 
 	Teachers
 	WHERE Phone LIKE '%72%'
+
+
+--- 5.Teen Students ---
+SELECT 
+	FirstName, 
+	LastName,
+	Age
+	FROM Students
+	WHERE Age >= 12
+	ORDER BY FirstName, LastName
+
+
+--- 6.Cool Addresses ---
+SELECT 
+	CASE
+		WHEN MiddleName IS NULL THEN CONCAT(FirstName, ' ', LastName)
+		ELSE CONCAT(FirstName, ' ', MiddleName, ' ', LastName)
+		END AS [Full Name],
+		Address
+	FROM Students
+	WHERE Address LIKE '%road%'
+	ORDER BY FirstName, LastName, Address
+
+
+--- 7.42 Phones ---
+SELECT 
+	FirstName,
+	Address,
+	Phone
+	FROM Students
+	WHERE Phone LIKE '42%' AND MiddleName IS NOT NULL
+	ORDER BY FirstName
+
+
+--- 8. Students Teachers ---
+SELECT 
+	s.FirstName,
+	s.LastName,
+	COUNT(st.TeacherId) AS TeachersCount
+	FROM Students as s
+	LEFT JOIN StudentsTeachers as st ON s.Id = st.StudentId
+	GROUP BY s.FirstName, s.LastName
+
+
+--- 9.Subjects with Students ---
+SELECT 
+	CONCAT(Teachers.FirstName, ' ', Teachers.LastName) AS FullName,
+	CONCAT(Subjects.Name, '-', Subjects.Lessons) AS Subjects,
+	COUNT(Students.Id) AS Students
+	FROM Subjects
+	JOIN Teachers ON Subjects.Id = Teachers.SubjectId
+	JOIN StudentsTeachers ON Teachers.Id = StudentsTeachers.TeacherId
+	JOIN Students ON StudentsTeachers.StudentId = Students.Id 
+	GROUP BY CONCAT(Teachers.FirstName, ' ', Teachers.LastName), CONCAT(Subjects.Name, '-', Subjects.Lessons)
+	ORDER BY Students DESC
+
+
+--- 10.Students to Go ---
