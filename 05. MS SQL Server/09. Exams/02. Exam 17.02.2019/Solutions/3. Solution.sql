6f79bf8574be80bf1be1c6b3ac9a22e7abbbd978
Georgi Gradev <georgi.d.gradev@gmail.com>
2021-01-15 17:08:44 +0200

    Update 3. Solution.sql

--- 05. MS SQL Server/09. Exams/02. Exam 17.02.2019/Solutions/3. Solution.sql
+++ 05. MS SQL Server/09. Exams/02. Exam 17.02.2019/Solutions/3. Solution.sql
@@ -281,6 +281,7 @@ CREATE OR ALTER PROCEDURE usp_ExcludeFromSchool(@StudentId INT)
 AS
 	IF(SELECT Id FROM Students WHERE Id = @StudentId) IS NULL
 	THROW 50001, 'This school has no student with the provided id!', 1
+	RETURN
 
 	DELETE FROM StudentsExams WHERE StudentId = @StudentId
 	DELETE FROM StudentsSubjects WHERE StudentId =  @StudentId
