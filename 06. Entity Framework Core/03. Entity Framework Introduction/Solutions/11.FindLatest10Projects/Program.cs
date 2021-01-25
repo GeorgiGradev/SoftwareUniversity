d334fb06894532a810943fb2f5117827b18cc297
Georgi Gradev <georgi.d.gradev@gmail.com>
2021-01-25 17:28:06 +0200

    Create Program.cs

---
+++ 06. Entity Framework Core/03. Entity Framework Introduction/Solutions/11.FindLatest10Projects/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _11.FindLatest10Projects
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
