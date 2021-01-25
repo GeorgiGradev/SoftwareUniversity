b6dd7be9bf1796db9f37cfb71dd7f56616e82da2
Georgi Gradev <georgi.d.gradev@gmail.com>
2021-01-25 17:27:48 +0200

    Create Program.cs

---
+++ 06. Entity Framework Core/03. Entity Framework Introduction/Solutions/14.DeleteProjectById/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _14.DeleteProjectById
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
