64ea0a68cd85ca64ea0e47e1e04a7e3bea8dcf08
Georgi Gradev <georgi.d.gradev@gmail.com>
2021-01-25 17:27:58 +0200

    Create Program.cs

---
+++ 06. Entity Framework Core/03. Entity Framework Introduction/Solutions/12.IncreaseSalaries/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _12.IncreaseSalaries
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
