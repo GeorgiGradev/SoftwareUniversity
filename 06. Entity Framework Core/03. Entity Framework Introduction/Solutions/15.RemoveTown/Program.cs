f634b9254cadf483ab8931d62d14878d996e4d1a
Georgi Gradev <georgi.d.gradev@gmail.com>
2021-01-25 17:27:40 +0200

    Create Program.cs

---
+++ 06. Entity Framework Core/03. Entity Framework Introduction/Solutions/15.RemoveTown/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _15.RemoveTown
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
