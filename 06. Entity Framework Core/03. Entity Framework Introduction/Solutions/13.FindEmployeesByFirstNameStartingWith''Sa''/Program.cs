34f167fa97cead49ffcd3fc7fbb14215cfc536b7
Georgi Gradev <georgi.d.gradev@gmail.com>
2021-01-25 17:27:52 +0200

    Create Program.cs

---
+++ 06. Entity Framework Core/03. Entity Framework Introduction/Solutions/13.FindEmployeesByFirstNameStartingWith''Sa''/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _13.FindEmployeesByFirstNameStartingWith__Sa__
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
