cb8ca911dad34ef6b8d487aa295999a8ae1809ee
Georgi Gradev <georgi.d.gradev@gmail.com>
2021-01-25 17:28:58 +0200

    Create Program.cs

---
+++ 06. Entity Framework Core/03. Entity Framework Introduction/Solutions/03.EmployeesFullInformation/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _03.EmployeesFullInformation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
