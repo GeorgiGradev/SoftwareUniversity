fb9c5e12ecf8f88590943025066f60682f8de4d6
Georgi Gradev <georgi.d.gradev@gmail.com>
2021-01-25 17:29:11 +0200

    Create Program.cs

---
+++ 06. Entity Framework Core/03. Entity Framework Introduction/Solutions/02.DatabaseFirst/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _02.DatabaseFirst
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
