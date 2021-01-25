65635640b8ea0766e71f24339e75a5d7bbca2563
Georgi Gradev <georgi.d.gradev@gmail.com>
2021-01-25 17:28:32 +0200

    Create Program.cs

---
+++ 06. Entity Framework Core/03. Entity Framework Introduction/Solutions/08.AddressesByTown/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _08.AddressesByTown
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
