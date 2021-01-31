18886ad57ba1ae28505a152472cc9b1fb3884587
Georgi Gradev <georgi.d.gradev@gmail.com>
2021-01-31 17:13:13 +0200

    Create StartUp.cs

---
+++ 06. Entity Framework Core/05. LINQ/Solutions/P01_HospitalDatabase/StartUp.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace P01_HospitalDatabase
+{
+    class StartUp
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
