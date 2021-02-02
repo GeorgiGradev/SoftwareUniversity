c55c09adfb510ecb646c87d4f6e1d69880475160
Georgi Gradev <georgi.d.gradev@gmail.com>
2021-02-02 14:57:46 +0200

    Create 3. Solution.sql

---
+++ 05. MS SQL Server/09. Exams/01. Exam 20.10.2019 - Service/My Solutions/3. Solution.sql
@@ -0,0 +1,28 @@
+--- 02.Insert ---
+INSERT INTO Employees(FirstName, LastName, Birthdate, DepartmentId)
+	VALUES
+	('Marlo', 'O''Malley', '1958-9-21', 1),
+	('Niki', 'Stanaghan', '1969-11-26', 4),
+	('Ayrton', 'Senna', '1960-03-21', 9),
+	('Ronnie', 'Peterso', '1944-02-14',	9),
+	('Giovanna', 'Amati', '1959-07-20',	5)
+
+INSERT INTO Reports(CategoryId, StatusId, OpenDate, CloseDate, Description, UserId, EmployeeId)
+	VALUES
+(1, 1, '2017-04-13', NULL,	'Stuck Road on Str.133', 6,	2),
+(6, 3, '2015-09-05', '2015-12-06',	'Charity trail running', 3,	5),
+(1, 2, '2015-09-07', NULL,	'Falling bricks on Str.58',	5, 2),
+(4, 3, '2017-07-03', '2017-07-06',	'Cut off streetlight on Str.11', 1,	1)
+
+--- 03.Update ---
+UPDATE Reports
+	SET CloseDate = GETDATE()
+	WHERE CloseDate is Null
+
+--- 04.Delete ---
+DELETE
+	FROM Reports
+	WHERE StatusId = 4;
+
+--- 05.Unassigned Reports---
+
