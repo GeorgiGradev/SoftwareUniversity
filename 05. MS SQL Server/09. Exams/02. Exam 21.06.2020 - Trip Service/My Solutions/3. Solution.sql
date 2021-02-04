b533d3582fc7881d7a9348572d1b3597abe30532
Georgi Gradev <georgi.d.gradev@gmail.com>
2021-02-04 16:37:23 +0200

    Update 3. Solution.sql

--- 05. MS SQL Server/09. Exams/02. Exam 21.06.2020 - Trip Service/My Solutions/3. Solution.sql
+++ 05. MS SQL Server/09. Exams/02. Exam 21.06.2020 - Trip Service/My Solutions/3. Solution.sql
@@ -197,4 +197,35 @@ SELECT dbo.udf_GetAvailableRoom(112, '2011-12-17', 2)
 SELECT dbo.udf_GetAvailableRoom(94, '2015-07-26', 3)
 
 
---- 12. Switch Room --- 
+--- 12.Switch Room --- 
+GO
+CREATE PROC usp_SwitchRoom (@TripId INT, @TargetRoomId INT)
+AS
+BEGIN
+	DECLARE	@CurrentRoom INT = (SELECT TOP(1) RoomId FROM Trips WHERE Id = @TripId)
+
+	IF (SELECT TOP(1) HotelId FROM Rooms WHERE Id = @CurrentRoom) !=
+	   (SELECT TOP(1) HotelId FROM Rooms WHERE Id = @TargetRoomId)
+			THROW 51000, 'Target room is in another hotel!', 1
+
+	
+	IF (SELECT Beds FROM Rooms WHERE Id = @TargetRoomId) <
+	   (
+	    SELECT 
+			COUNT(*)
+			FROM Trips AS t
+			JOIN AccountsTrips AS at ON t.Id = at.TripId
+			WHERE t.Id = @TripId
+			GROUP BY t.Id
+		   )
+				THROW 51001, 'Not enough beds in target room!', 1
+
+	   UPDATE Trips
+	   SET RoomId = @TargetRoomId
+	   WHERE Id = @TripId
+END
+
+GO
+
+EXEC usp_SwitchRoom 10, 7
+EXEC usp_SwitchRoom 10, 8
