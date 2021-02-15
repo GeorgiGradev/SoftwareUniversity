81d2d6e72adbf0762cdcc2b11162484a72d29643
Georgi Gradev <georgi.d.gradev@gmail.com>
2021-02-15 12:00:55 +0200

    Update User.cs

--- 06. Entity Framework Core/04. Entity Relations/Solutions/P03_FootballBetting/P03_FootballBetting.Data.Models/User.cs
+++ 06. Entity Framework Core/04. Entity Relations/Solutions/P03_FootballBetting/P03_FootballBetting.Data.Models/User.cs
@@ -10,7 +10,7 @@ namespace P03_FootballBetting.Data.Models
         }
 
         public int UserId { get; set; }
-        public string UserName { get; set; }
+        public string Username { get; set; }
         public string Password { get; set; }
         public string Email { get; set; }
         public string Name { get; set; } 
